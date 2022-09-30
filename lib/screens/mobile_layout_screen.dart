4042a5d8f511c1a8db3e953a2e1942097195b12d
Avivar101 <63792174+Avivar101@users.noreply.github.com>
2022-09-30 12:05:48 +0100

    commit

--- lib/screens/mobile_layout_screen.dart
+++ lib/screens/mobile_layout_screen.dart
@@ -1,12 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:whatsapp_flutter_ui/widgets/contacts_list.dart';
+
+import '../constants.dart';
 
 class MobileLayoutScreen extends StatelessWidget {
   const MobileLayoutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Text('Mobile!'),
+    return DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: appBarColor,
+            centerTitle: false,
+            title: const Text(
+              'WhatsApp',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold
+              )
+            ),
+            actions: [
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.search, color: Colors.grey)
+              ),
+              IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert, color: Colors.grey)
+              )
+            ],
+            bottom: const TabBar(
+              indicatorColor: tabColor,
+              indicatorWeight: 4,
+              labelColor: tabColor,
+              unselectedLabelColor: Colors.grey,
+              labelStyle: TextStyle(
+                fontWeight: FontWeight.bold
+              ),
+              tabs: [
+                Tab(text: 'CHATS',),
+                Tab(text: 'STATUS',),
+                Tab(text: 'CALLS',)
+              ],
+            ),
+          ),
+          body: const ContactList(),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: tabColor,
+            child: const Icon(
+              Icons.comment,
+              color: Colors.white
+            ),
+          ),
+        )
     );
   }
 }
